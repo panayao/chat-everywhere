d2dd84a330a98045cafb9571fee8356784400a2f
Jack wu <jackwu833016@gmail.com>
2023-06-01 17:00:04 -0400

    Increate GPT4 usages to 50

--- utils/config.ts
+++ utils/config.ts
@@ -3,6 +3,6 @@ import { PluginID } from '@/types/plugin';
 // Amount of credit will be added to all user's account on the 1st day of every month
 export const DefaultMonthlyCredits = {
   [PluginID.LANGCHAIN_CHAT]: 0,
-  [PluginID.GPT4]: 25,
+  [PluginID.GPT4]: 50,
   [PluginID.IMAGE_GEN]: 50,
 }
